b9e4c0c876ba492a005071eea574f74ffa8f7ba6
Xenos <acristyano22@gmail.com>
2024-09-12 15:10:14 +0100

    fix: some dark mode issues with the `input` component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -10,12 +10,14 @@ export default function Input({
         {...props}
         placeholder={props.type === "number" ? "20" : "rgba(0, 0, 0, 0.1)"}
         className={cn(
-          "rounded-sm border border-black/20 px-2 outline-offset-4 focus:border-border-color focus:outline-1",
+          "dark:border-border-color-dark rounded-sm border border-black/20 px-2 outline-offset-4 focus:border-border-color focus:outline-1 dark:bg-black",
           props.className,
         )}
       />
       {props.type === "number" ? (
-        <span className="absolute translate-x-[-120%] text-black/40">px</span>
+        <span className="absolute translate-x-[-120%] text-black/40 dark:text-white/40">
+          px
+        </span>
       ) : null}
     </div>
   );
