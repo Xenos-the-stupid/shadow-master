2fbbaf9fab4bc3615881e16040264026c2a1b450
Xenos <acristyano22@gmail.com>
2024-09-14 12:57:49 +0100

    remove server integration

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,18 +5,10 @@ import generateProperty from "../utils/generate-property";
 import { boxShadowAtom } from "../store/boxShadow";
 import { useAtom } from "jotai";
 import toast from "react-hot-toast";
-import Modal from "./ui/Modal";
-import Input from "./ui/Input";
-import { useState } from "react";
-import TextAria from "./ui/TextAria";
-const API_URL = import.meta.env.DEV
-  ? "http://localhost:3000"
-  : import.meta.env.VITE_API_URL!;
 
 export default function Header() {
   const { themeMode, setThemeMode } = useThemeMode();
   const [boxShadow] = useAtom(boxShadowAtom);
-  const [feedBack, setFeedBack] = useState({ title: "", message: "" });
 
   const copyGeneratedCode = () => {
     const generatedCode = generateProperty(boxShadow);
@@ -27,66 +19,10 @@ export default function Header() {
     });
   };
 
-  const submit = async () => {
-    try {
-      const res = await fetch(API_URL, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(feedBack),
-      });
-      if (res.status !== 200) throw new Error("Something went wrong");
-      toast.success("Thank you for your feedback", {
-        className:
-          "dark:bg-black dark:text-primary-text shadow-md dark:text-primary-text-dark dark:shadow-white/30",
-      });
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    } catch (_e) {
-      toast.error("Something went wrong", {
-        className:
-          "dark:bg-black dark:text-primary-text shadow-md dark:text-primary-text-dark dark:shadow-white/30",
-      });
-    }
-  };
-
   return (
     <header className="sticky top-0 flex h-[90px] items-center justify-between border-b border-border-color px-[35px] dark:border-border-color-dark dark:bg-[#121212] dark:text-white">
       <h1 className="text-2xl font-medium">Shadow Master</h1>
       <div className="flex items-center gap-[1.625rem]">
-        <Modal>
-          <Modal.Trigger>FeedBack</Modal.Trigger>
-          <Modal.Body>
-            <Modal.Title>Your FeedBack</Modal.Title>
-            <p className="text-sm">
-              please fill out this form if you have any feedback about things we
-              should add, fixes we could fix
-            </p>
-            <Input
-              placeholder="Title"
-              value={feedBack.title}
-              onChange={(e) =>
-                setFeedBack({ ...feedBack, title: e.target.value })
-              }
-              className="w-full rounded-md px-4 py-2 text-xl focus-within:outline-none"
-            />
-            <TextAria
-              placeholder="Message"
-              onChange={(e) =>
-                setFeedBack({ ...feedBack, message: e.target.value })
-              }
-            />
-            <div className="flex justify-end">
-              <button
-                onClick={submit}
-                className="rounded-md bg-[#AED6F1] px-5 py-3 dark:text-black"
-              >
-                Submit
-              </button>
-            </div>
-          </Modal.Body>
-        </Modal>
-
         <a href="#">
           <FaGithub size={25} />
         </a>
